21b34e5b7d1b35aaa1149312d8b401aceb08b539
truder173 <uln-al-bar@yandex.ru>
2019-03-25 08:46:53 +0400

    null

---
+++ src/main/java/com/baranov/javacourse/Application.java
@@ -0,0 +1,12 @@
+package com.baranov.javacourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
